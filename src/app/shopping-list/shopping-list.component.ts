255f82b3be56fe0a1f476674e6725818a4c5a131
Jon Erickson <jon.erickson@sportngin.com>
2018-11-15 14:41:27 -0600

    filling in the components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 15),
+    new Ingredient('Potatoes', 5),
+    new Ingredient('Carrots', 15),
+  ]
 
   constructor() { }
 
