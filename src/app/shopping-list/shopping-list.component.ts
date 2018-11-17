7a10e06ef98c7518e43667e1d530b7125e928f25
Jon Erickson <jon.erickson@sportngin.com>
2018-11-17 13:05:36 -0600

    user observable subject to detect list changes, rather than event emitters

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,26 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  subscription: Subscription
 
   constructor(private shopplingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shopplingListService.getIngredients();
-    this.shopplingListService.ingredientsChanged
+    this.subscription = this.shopplingListService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
       )
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   onIngredientAdded(ingredient: Ingredient) {
